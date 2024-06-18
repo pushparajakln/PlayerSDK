90f6bdb78c211308319866cd7a3569cc17c55c50
Pushparaj <pushparaja.sivalingam@setindia.com>
2024-06-18 20:46:54 +0530

    PlayerSDK

---
+++ PlayerSDKLib/Sources/PlayerSDKLib/SonyAVPlayer.swift
@@ -0,0 +1,18 @@
+//
+//  SonyAVPlayer.swift
+//
+//
+//  Created by Pushparaja Sivalingam on 18/06/24.
+//
+
+import Foundation
+
+public struct SonyAVPlayer {
+    public init() {
+        
+    }
+    
+    public func playerData() {
+        print("SonyAVPlayer")
+    }
+}
